e6cca96fa6740ee2e3ff950635dcbc1ae74ba876
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2019-08-05 12:42:26 +0530

    added

---
+++ client/components/Landing/chooseTemplate.js
@@ -0,0 +1,49 @@
+import React, {Component, Fragment} from 'react';
+
+class ChooseTemplate extends Component{
+      render(){
+          return (
+                  <div className="row mb-6">
+                           <div className="col-md-12 text-center mb-5">
+                                  <h2 className="text-uppercase font-weight-bolder">The right <span className="text-info">Template</span> for the job</h2>
+                           </div>
+                           <div className="col-md-12">
+                                    <div className="slick-slider-2">
+                                        <div>
+                                            <div className="slider-item">1</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">2</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">3</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">4</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">5</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">6</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">7</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">8</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">9</div>
+                                        </div>
+                                        <div>
+                                            <div className="slider-item">10</div>
+                                        </div>
+                                    </div>
+                            </div>
+             </div>
+          )
+      }
+}
+
+export default ChooseTemplate;
--- template.js
+++ template.js
@@ -9,6 +9,7 @@ export default () => {
             <meta name="viewport"  content="width=device-width initial-scale=1"/>
         </head>
         <body>
+          <script type="text/javascript" src="/js/main.js"></script>
           <div id="root"></div>
           <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
